6cc41f0c8d8202de5ff37eb4e18bd54a5c46e4f9
Wesley Hayutin <whayutin@redhat.com>
2021-12-31 14:33:09 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/bmizerany/pat"
+)
+
+func main() {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(home))
+	mux.Post("/", http.HandlerFunc(send))
+	mux.Get("/confirmation", http.HandlerFunc(confirmation))
+
+	log.Println("Listening...")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	render(w, "templates/home.html", nil)
+}
+
+func send(w http.ResponseWriter, r *http.Request) {
+	// Step 1: Validate form
+	msg := &Message{
+		Name:       r.PostFormValue("name"),
+		Phone:      r.PostFormValue("phone"),
+		Email:      r.PostFormValue("email"),
+		Restaurant: r.PostFormValue("restaurant"),
+		DateTime:   r.PostFormValue("datetime"),
+		Content:    r.PostFormValue("content"),
+		Errors:     map[string]string{},
+	}
+
+	if msg.Validate() == false {
+		render(w, "templates/home.html", msg)
+		return
+	}
+
+	// Step 2: Send contact form message in an email
+	if err := msg.Deliver(); err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	// Step 3: Redirect to confirmation page
+	http.Redirect(w, r, "/confirmation", http.StatusSeeOther)
+}
+
+func confirmation(w http.ResponseWriter, r *http.Request) {
+	render(w, "templates/confirmation.html", nil)
+}
+
+func render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Println(err)
+		http.Error(w, "Sorry, something went wrong", http.StatusInternalServerError)
+	}
+}
---
+++ message.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/go-mail/mail"
+)
+
+var rxEmail = regexp.MustCompile(".+@.+\\..+")
+var rxPhone = regexp.MustCompile(`^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$`)
+
+type Message struct {
+	Name       string
+	DateTime   string
+	Phone      string
+	Email      string
+	Restaurant string
+	Content    string
+	Errors     map[string]string
+}
+
+func (msg *Message) Validate() bool {
+	msg.Errors = make(map[string]string)
+
+	match := rxEmail.Match([]byte(msg.Email))
+	if match == false {
+		msg.Errors["Email"] = "Please enter a valid email address"
+	}
+
+	if strings.TrimSpace(msg.Name) == "" {
+		msg.Errors["Name"] = "Please enter your name"
+	}
+
+	if strings.TrimSpace(msg.DateTime) == "" {
+		msg.Errors["Name"] = "Please enter the date and time of pickup"
+	}
+
+	if strings.TrimSpace(msg.Restaurant) == "" {
+		msg.Errors["Restaurant"] = "Please enter the Restaurant"
+	}
+
+	if strings.TrimSpace(msg.Content) == "" {
+		msg.Errors["Content"] = "Please enter a message"
+	}
+
+	matchPhone := rxPhone.Match([]byte(msg.Phone))
+	if matchPhone == false {
+		msg.Errors["Phone"] = "Please enter a valid phone number"
+	}
+
+	return len(msg.Errors) == 0
+}
+
+func (msg *Message) Deliver() error {
+	this_msg := "\n" +
+		"Name: " + msg.Name + "\n" +
+		"DateTime: " + msg.DateTime + "\n" +
+		"Phone: " + msg.Phone + "\n" +
+		"Email: " + msg.Email + "\n" +
+		"Restaurant: " + msg.Restaurant + "\n" +
+		"Content: " + msg.Content
+
+	email := mail.NewMessage()
+	email.SetHeader("To", os.Getenv("boazform_emailto"))
+	email.SetHeader("From", os.Getenv("boazform_from"))
+	email.SetHeader("Reply-To", msg.Email)
+	email.SetHeader("Subject", "New message via Contact Form")
+	email.SetBody("text/plain", this_msg)
+
+	username := os.Getenv("boazform_username")
+	password := os.Getenv("boazform_password")
+
+	return mail.NewDialer("smtp.gmail.com", 465, username, password).DialAndSend(email)
+}
